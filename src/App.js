fe4813b4afc484776af61315b57ee4db16f716d5
MYEONGJIN-LEE <myeongjin3203@gmail.com>
2021-07-19 14:11:27 +0900

    React Router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Link, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+import Profiles from "./Profiles";
+import HistorySample from "./HistorySample";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <ul>
+                <li>
+                    <Link to="/">홈</Link>
+                </li>
+                <li>
+                    <Link to="/about">소개</Link>
+                </li>
+                <li>
+                    <Link to="/profiles">프로필 목록</Link>
+                </li>
+                <li>
+                    <Link to="/history">예제</Link>
+                </li>
+            </ul>
+            <hr></hr>
+            <Switch>
+                <Route path="/" exact={true} component={Home} />
+                <Route path="/about" component={About} />
+                <Route path="/profiles/" component={Profiles} />
+                <Route path="/history" component={HistorySample} />
+                <Route
+                    // path 를 따로 정의하지 않으면 모든 상황에 렌더링됨
+                    render={({ location }) => (
+                        <div>
+                            <h2>이 페이지는 존재하지 않습니다:</h2>
+                            <p>{location.pathname}</p>
+                        </div>
+                    )}
+                />
+            </Switch>
+        </div>
+    );
 }
 
 export default App;
